c4f63ce61d7b730f8ed05a7cf8ab5087680fb394
Federico Carrone <federicocarrone@pampa.fibertel.com.ar>
2022-05-02 08:42:49 -0300

    Update ABI

--- eth_client/lib/eth_client/abi.ex
+++ eth_client/lib/eth_client/abi.ex
@@ -1,13 +1,10 @@
 defmodule EthClient.ABI do
 
-    use Tesla
-    plug(Tesla.Middleware.Headers, [{"content-type", "application/json"}])
+    def get(address), do: get_etherscan(address)
 
-    def get(address) do
+    def get_etherscan(address) do
         api_key = Application.fetch_env!(:eth_client, :etherscan)
         url = "https://api.etherscan.io/api?module=contract&action=getabi&address=#{address}&apikey=#{api_key}"
-
-        IO.inspect(url)
 
         {:ok, rsp} = Tesla.get(url)
         handle_response(rsp)
