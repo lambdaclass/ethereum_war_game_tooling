66520e4a47a4b0868ff0443efa64e52c8445481f
Francisco Krause Arnim <fkrausear@gmail.com>
2022-05-17 16:14:34 -0300

    Add function to turn contract into opcode:
    - Add function to print the corrent context contract into opcodes
    - Add function to print the opcodes of a given contract via its address.

--- eth_client/lib/eth_client/contract.ex
+++ eth_client/lib/eth_client/contract.ex
@@ -2,6 +2,8 @@ defmodule EthClient.Contract do
   @moduledoc false
 
   alias EthClient.ABI
+  alias EthClient.Rpc
+  alias EthClient.Contract.Opcodes
 
   defstruct [:address, :functions]
 
@@ -10,6 +12,14 @@ defmodule EthClient.Contract do
   def get_functions(address_or_path) do
     with {:ok, abi} <- ABI.get(address_or_path) do
       parse_abi(abi)
+    end
+  end
+
+  def to_opcodes, do: EthClient.Context.contract().address |> contract_to_opcodes
+
+  def contract_to_opcodes(address) when is_binary(address) do
+    with {:ok, code} <- Rpc.get_code(address) do
+      Opcodes.bytecode_to_opcodes(code)
     end
   end
 
