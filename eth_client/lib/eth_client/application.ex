f3329d6a519c82c012a35b00d6dc4da57ed54a35
mfachal <647731+mfachal@users.noreply.github.com>
2022-05-19 18:57:27 -0300

    extract initial context creation into a different function

--- eth_client/lib/eth_client/application.ex
+++ eth_client/lib/eth_client/application.ex
@@ -10,18 +10,7 @@ defmodule EthClient.Application do
 
   @impl true
   def start(_type, _args) do
-    {chain_id, _} = Integer.parse(System.get_env("ETH_CHAIN_ID", "1234"))
-
-    initial_context = %{
-      chain_id: chain_id,
-      rpc_host: System.get_env("ETH_RPC_HOST", "http://localhost:8545"),
-      user_account: %Account{
-        address: System.get_env("ETH_USER_ADDRESS", "0xafb72ccaeb7e22c8a7640f605824b0898424b3da"),
-        private_key: System.get_env("ETH_USER_PK", "e90d75baafee04b3d9941bd8d76abe799b391aec596515dee11a9bd55f05709c"),
-      },
-      etherscan_api_key: System.get_env("ETH_API_KEY", nil),
-      contract: %Contract{address: System.get_env("ETH_CONTRACT", nil), functions: nil}
-    }
+    initial_context = get_initial_context()
 
     children = [
       {EthClient.Context, initial_context}
@@ -31,5 +20,22 @@ defmodule EthClient.Application do
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: EthClient.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  def get_initial_context() do
+    {chain_id, _} = "ETH_CHAIN_ID"
+         |> System.fetch_env!()
+         |> Integer.parse()
+
+    %{
+      chain_id: chain_id,
+      rpc_host: System.fetch_env!("ETH_RPC_HOST"),
+      user_account: %Account{
+        address: System.fetch_env!("ETH_USER_ADDRESS"),
+        private_key: System.fetch_env!("ETH_USER_PK"),
+      },
+      etherscan_api_key: System.fetch_env!("ETH_API_KEY"),
+      contract: %Contract{address: System.fetch_env!("ETH_CONTRACT"), functions: nil}
+    }
   end
 end
