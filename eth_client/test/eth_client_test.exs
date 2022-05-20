ce3616e4757a1097fdad67969d800295466a5a34
Milton Scuderi <milton.scuderi@lambdaclass.com>
2022-05-20 12:36:13 -0300

    Test scripts for ethclient.ex

--- eth_client/test/eth_client_test.exs
+++ eth_client/test/eth_client_test.exs
@@ -1,4 +1,48 @@
 defmodule EthClientTest do
   use ExUnit.Case
   doctest EthClient
+  alias EthClient.Context
+  alias EthClient.Account
+
+  @bin "../contracts/src/bin/Storage.bin"
+  @abi "../contracts/src/bin/Storage.abi"
+
+  describe "Ethereum war tooling functions" do
+    test "Deploy" do
+      bin = @bin
+      abi = @abi
+
+      contract = EthClient.deploy(bin, abi)
+      assert contract == EthClient.Context.contract()
+    end
+
+    test "Invoke" do
+      bin = @bin
+      abi = @abi
+
+      contract = EthClient.deploy(bin, abi)
+      {:ok, tx_ans} = EthClient.invoke("store(uint256)", [3], 0)
+      assert tx_ans != nil
+    end
+
+    test "Call" do
+      bin = @bin
+      abi = @abi
+
+      contract = EthClient.deploy(bin, abi)
+      {:ok, res} = EthClient.call("retrieve()", [])
+      assert res == "0x0000000000000000000000000000000000000000000000000000000000000000"
+      {:ok, res} = EthClient.call("test_function()", [])
+      assert res == "0x0000000000000000000000000000000000000000000000000000000000000001"
+    end
+
+    test "Balance" do
+      bin = @bin
+      abi = @abi
+
+      contract = EthClient.deploy(bin, abi)
+
+      assert 0.0 == EthClient.get_balance(contract.address)
+    end
+  end
 end
