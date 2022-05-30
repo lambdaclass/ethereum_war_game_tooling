a1058c5b462f99fb1e9b9006ddc3e942e575d279
jrigada <jrigada@frba.utn.edu.ar>
2022-05-30 10:07:49 -0300

    remove tags

--- eth_client/test/eth_client_test.exs
+++ eth_client/test/eth_client_test.exs
@@ -6,37 +6,33 @@ defmodule EthClientTest do
   @abi "../contracts/src/bin/Storage.abi"
 
   setup_all do
+    IO.inspect(EthClient.Context.all())
     contract = EthClient.deploy(@bin, @abi)
     {:ok, contract: contract}
   end
 
   describe "deploy/2" do
-    @tag bin: @bin, abi: @abi
     test "[SUCCESS] Succesful deploy", %{contract: contract} do
       assert contract == EthClient.Context.contract()
     end
   end
 
   describe "invoke/3" do
-    @tag bin: @bin, abi: @abi
     test "[SUCCESS] invokes a function it returns the transaction hash" do
       {:ok, tx_ans} = EthClient.invoke("store(uint256)", [3], 0)
       assert tx_ans != nil
     end
 
-    @tag bin: @bin, abi: @abi
     test "[FAILURE] invokes a function with incorrect amount of params" do
       assert_raise FunctionClauseError, fn -> EthClient.invoke("store(uint256)", []) end
     end
 
-    @tag bin: @bin, abi: @abi
     test "[FAILURE] invokes a contract function with incorrect params" do
       assert_raise FunctionClauseError, fn -> EthClient.invoke("store(uint256)", [], 0) end
     end
   end
 
   describe "call/2" do
-    @tag bin: @bin, abi: @abi
     test "[SUCCESS] Call" do
       {:ok, res} = EthClient.call("retrieve()", [])
       assert res == "0x0000000000000000000000000000000000000000000000000000000000000003"
@@ -44,24 +40,20 @@ defmodule EthClientTest do
       assert res == "0x0000000000000000000000000000000000000000000000000000000000000001"
     end
 
-    @tag bin: @bin, abi: @abi
     test "[FAILURE] calls a function with incorrect amount of params" do
       assert_raise UndefinedFunctionError, fn -> EthClient.call("retrieve()") end
     end
 
-    @tag bin: @bin, abi: @abi
     test "[FAILURE] calls a contract function with incorrect params" do
       assert_raise FunctionClauseError, fn -> EthClient.call("retrieve()", [12]) end
     end
   end
 
   describe "balance/1" do
-    @tag bin: @bin, abi: @abi
     test "[SUCCESS] Balance", %{contract: contract} do
       assert 0.0 == EthClient.get_balance(contract.address)
     end
 
-    @tag bin: @bin, abi: @abi
     test "[FAILURE] unexisting address", %{contract: _contract} do
       assert_raise FunctionClauseError, fn -> EthClient.get_balance("0x123213b") end
     end
