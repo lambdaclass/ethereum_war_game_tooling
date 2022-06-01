b7cc3e4ef5201354c8c3e1896fcf6c19a5bf82e8
Estefi <estefi@camba.coop>
2022-06-01 17:29:09 -0300

    wip: abi tests

--- eth_client/test/abi_test.exs
+++ eth_client/test/abi_test.exs
@@ -7,10 +7,12 @@ defmodule EthClientTest.ABI do
   @bin "../contracts/src/bin/Storage.bin"
   @abi "../contracts/src/bin/Storage.abi"
 
-  # setup_all do
-  #   EthClient.deploy(@bin, @abi)
-  #   :ok
-  # end
+  setup_all do
+    # Context.set_chain_id(4)
+    # Context.set_rpc_host("")
+    contract = EthClient.deploy(@bin, @abi)
+    :ok
+  end
 
   describe "get/1" do
     @tag bin: @bin, abi: @abi
@@ -53,41 +55,21 @@ defmodule EthClientTest.ABI do
     @tag bin: @bin, abi: @abi
     test "[SUCCESS] Get an ABI by an ABI address" do
 
-      contract = Context.contract()
-      address = contract.address
+      address = Context.user_account().address
 
-      result = ABI.get(address)
+      {code, _response} = ABI.get(address)
 
-      assert {:ok,
-         [
-           %{
-             "inputs" => [],
-             "name" => "retrieve",
-             "outputs" => [
-               %{"internalType" => "uint256", "name" => "", "type" => "uint256"}
-             ],
-             "stateMutability" => "view",
-             "type" => "function"
-           },
-           %{
-             "inputs" => [
-               %{"internalType" => "uint256", "name" => "num", "type" => "uint256"}
-             ],
-             "name" => "store",
-             "outputs" => [],
-             "stateMutability" => "nonpayable",
-             "type" => "function"
-           },
-           %{
-             "inputs" => [],
-             "name" => "test_function",
-             "outputs" => [
-               %{"internalType" => "uint256", "name" => "", "type" => "uint256"}
-             ],
-             "stateMutability" => "pure",
-             "type" => "function"
-           }
-         ]} == result
+      assert :ok == code
+    end
+
+    @tag bin: @bin, abi: @abi
+    test "[FAILURE] Get an ABI by an invalid ABI address" do
+
+      address = '0x0'
+
+      assert_raise MatchError, fn ->
+        ABI.get(address)
+      end
     end
   end
 
