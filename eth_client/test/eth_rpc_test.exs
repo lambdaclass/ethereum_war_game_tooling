075a965a9711d3f74d10c1e9296c28797778d368
Cristhian Cotes <cristhian9292@gmail.com>
2022-06-14 16:24:12 -0500

    credo

--- eth_client/test/eth_rpc_test.exs
+++ eth_client/test/eth_rpc_test.exs
@@ -93,17 +93,10 @@ defmodule EthClientTest.Rpc do
       assert %{
                "blockHash" => _blockhash,
                "blockNumber" => _blocknumber,
-               "contractAddress" => _contactAdress,
-               "cumulativeGasUsed" => _cumulativeGasUsed,
-               "effectiveGasPrice" => _effectiveGasPrice,
                "from" => _from,
-               "gasUsed" => _gasUser,
                "logs" => [],
-               "logsBloom" => _logsBloom,
                "status" => _status,
                "to" => nil,
-               "transactionHash" => _transactionHash,
-               "transactionIndex" => _transactionIndex,
                "type" => _type
              } = receipt_map
     end
