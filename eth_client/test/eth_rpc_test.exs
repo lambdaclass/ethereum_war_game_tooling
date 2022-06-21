6e2d615376c85cd49458eef3dc2450c619ee6e6c
Cristhian Cotes <cristhian9292@gmail.com>
2022-06-21 14:37:06 -0500

    rinkeby

--- eth_client/test/eth_rpc_test.exs
+++ eth_client/test/eth_rpc_test.exs
@@ -7,7 +7,7 @@ defmodule EthClientTest.Rpc do
   @bin_path "../contracts/src/bin/Storage.bin"
 
   def transaction_deploy do
-    :ok = EthClient.set_chain("local")
+    :ok = EthClient.set_chain("rinkeby")
     {:ok, data} = File.read(@bin_path)
     data = "0x" <> data
 
