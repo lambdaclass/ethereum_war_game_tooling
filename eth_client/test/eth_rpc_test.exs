d1ff8e55a992c4770b8b38c9229e3eff5b396bf1
Cristhian Cotes <cristhian9292@gmail.com>
2022-06-22 17:26:54 -0500

    f

--- eth_client/test/eth_rpc_test.exs
+++ eth_client/test/eth_rpc_test.exs
@@ -112,7 +112,7 @@ defmodule EthClientTest.Rpc do
     end
 
     test "[SUCCESS] Get Logs" do
-      assert {:ok, []} == Rpc.get_logs(%{})
+      assert {:ok, list} == Rpc.get_logs(%{})
     end
 
     test "[SUCCESS] Get Balance", state do
